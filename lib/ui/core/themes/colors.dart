bc5fc150ddf7164dc73676bd13b9b72c96e2fedb
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-11-10 16:59:21 +0100

    Add PkColors class for theme color management

---
+++ lib/ui/core/themes/colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class PkColors {
+
+  // buttons color
+  static const primary = Color(0xFFDC2B2B);
+  static const secondary = Color(0xFF272020);
+  static const disabled = Color(0xFF969696);
+  static const border = Color(0xFFEEEDED);
+  static const text = Color(0xFFEEEDED);
+
+  // inputs colors
+  static const tf_background = Color(0xFFEEEDED);
+  static const tf_text = Color(0xFF272020);
+  static const tf_text_hint = Color(0xFF969696);
+  static const tf_border_enabled = Color(0xFFDC2B2B);
+
+  // text
+  static const tx_text = Color(0xFF272020);
+  static const tx_text_alt = Color(0xFF969696);
+  static const tx_text_primary = Color(0xFFDC2B2B);
+  static const tx_dark_text = Color(0xFFEEEDED);
+
+  // message buuble item
+  static const mb_border = Color(0xFF969696);
+  static const mb_background = Color(0xFFF5F5F5);
+  static const mb_dark_background = Color(0xFF272020);  
+
+  // chooser (pókemon, avatar ...)
+
+
+  // card pokemon
+
+
+  
+
+}
